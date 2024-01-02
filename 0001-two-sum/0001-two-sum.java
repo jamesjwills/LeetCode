e5e2964bef97259727c199d4a61278887649711d
James Wills <68990914+jamesjwills@users.noreply.github.com>
2024-01-02 18:51:13 +0000

    Time: 88 ms (7.15%), Space: 45.4 MB (7.39%) - LeetHub

---
+++ 0001-two-sum/0001-two-sum.java
@@ -0,0 +1,31 @@
+import java.util.Arrays;
+
+public class Solution {
+    public int[] twoSum(int[] nums, int target) {
+        int[] result = new int[2];
+        for (int i = 0; i < nums.length; i++) {
+            int j = i + 1;
+            for (j = i + 1; j < nums.length; j++) {
+                if (nums[i] + nums[j] == target) {
+
+                    result[0] = i;
+                    result[1] = j;
+
+                }
+            }
+        }
+        return result;
+    }
+
+    public static void main(String[] args) {
+        Solution sln = new Solution();
+        int[] inputArray = new int[args.length - 1];
+
+        for (int i = 0; i < args.length-1; i++) {
+            inputArray[i] = Integer.parseInt(args[i]);
+        }
+
+        int[] result = sln.twoSum(inputArray, Integer.parseInt(args[args.length-1]));
+        System.out.println(Arrays.toString(result));
+    }
+}
